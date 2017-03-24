abc25a5f1e920f4f109adcf0deaf3848e798137f
David Lechner <david@lechnology.com>
2017-03-24 13:23:32 -0500

    Update release script for stretch

--- release.sh
+++ release.sh
@@ -7,16 +7,16 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 
-OS=debian DIST=jessie ARCH=armhf pbuilder-ev3dev build
-OS=debian DIST=jessie ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
-OS=raspbian DIST=jessie ARCH=armhf pbuilder-ev3dev build
+OS=debian DIST=stretch ARCH=armhf pbuilder-ev3dev build
+OS=debian DIST=stretch ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
+OS=raspbian DIST=stretch ARCH=armhf pbuilder-ev3dev build
 
-debsign ~/pbuilder-ev3dev/debian/jessie-armhf/${source}_${version}_armhf.changes
-debsign ~/pbuilder-ev3dev/debian/jessie-armel/${source}_${version}_armel.changes
-debsign ~/pbuilder-ev3dev/raspbian/jessie-armhf/${source}_${version}_armhf.changes
+debsign ~/pbuilder-ev3dev/debian/stretch-armhf/${source}_${version}_armhf.changes
+debsign ~/pbuilder-ev3dev/debian/stretch-armel/${source}_${version}_armel.changes
+debsign ~/pbuilder-ev3dev/raspbian/stretch-armhf/${source}_${version}_armhf.changes
 
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/jessie-armhf/${source}_${version}_armhf.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/jessie-armel/${source}_${version}_armel.changes
-dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/jessie-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/stretch-armel/${source}_${version}_armel.changes
+dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/stretch-armhf/${source}_${version}_armhf.changes
 
 gbp buildpackage --git-tag-only
