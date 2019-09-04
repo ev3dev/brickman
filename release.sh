adf95e07a66a47e1aa846ba50c9c713e59a31ddc
David Lechner <david@lechnology.com>
2019-09-03 21:11:53 -0500

    debian: drop codename from release script

--- release.sh
+++ release.sh
@@ -7,18 +7,17 @@ set -e
 source=$(dpkg-parsechangelog -S Source)
 version=$(dpkg-parsechangelog -S Version)
 distribution=$(dpkg-parsechangelog -S Distribution)
-codename=$(debian-distro-info --codename --${distribution})
 
-OS=debian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
-OS=debian DIST=${codename} ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
-OS=raspbian DIST=${codename} ARCH=armhf pbuilder-ev3dev build
+OS=debian DIST=${distribution} ARCH=armhf pbuilder-ev3dev build
+OS=debian DIST=${distribution} ARCH=armel PBUILDER_OPTIONS="--binary-arch" pbuilder-ev3dev build
+OS=raspbian DIST=${distribution} ARCH=armhf pbuilder-ev3dev build
 
-debsign ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
-debsign ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
-debsign ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
+debsign ~/pbuilder-ev3dev/debian/${distribution}-armhf/${source}_${version}_armhf.changes
+debsign ~/pbuilder-ev3dev/debian/${distribution}-armel/${source}_${version}_armel.changes
+debsign ~/pbuilder-ev3dev/raspbian/${distribution}-armhf/${source}_${version}_armhf.changes
 
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armhf/${source}_${version}_armhf.changes
-dput ev3dev-debian ~/pbuilder-ev3dev/debian/${codename}-armel/${source}_${version}_armel.changes
-dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${codename}-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${distribution}-armhf/${source}_${version}_armhf.changes
+dput ev3dev-debian ~/pbuilder-ev3dev/debian/${distribution}-armel/${source}_${version}_armel.changes
+dput ev3dev-raspbian ~/pbuilder-ev3dev/raspbian/${distribution}-armhf/${source}_${version}_armhf.changes
 
 gbp buildpackage --git-tag-only
